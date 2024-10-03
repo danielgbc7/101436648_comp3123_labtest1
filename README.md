10557f0c7234fd82f08061cf679b7da4dd75cd79
danielgbc7 <daniel.konjarski@georgebrown.ca>
2024-10-03 18:20:48 -0400

    Question 1 Solution

---
+++ README.md
@@ -0,0 +1,9 @@
+COMP3123 - Lab Test 1
+Full Stack Development 1
+Student ID: 101436648
+
+Project Overview
+
+This project consists of solutions to the lab test for Full Stack Development I (COMP 3123). The solutions cover topics such as ES6 features, promises, and file handling in Node.js.
+
+Each task is implemented in separate scripts to ensure a clean and modular structure, as outlined in the test instructions.
--- question-1/question-1.js
+++ question-1/question-1.js
@@ -1,2 +1,20 @@
 // Daniel Konjaski - 101436648
 // COMP3123 - Labtest 1
+
+function lowerCaseWords(arr) {
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(arr)) {
+            reject("Input must be an array");
+        } else {
+            const result = arr
+                .filter(item => typeof item === 'string')
+                .map(item => item.toLowerCase());
+            resolve(result);
+        }
+    });
+}
+
+const mixedArray = [10, 'Full', 3123, 'STACK', true, 'LabTest', false, 12345, 'OnE'];
+lowerCaseWords(mixedArray)
+    .then(result => console.log(result))
+    .catch(error => console.error(error));
